8b702d908f15306235a0379747dd030df970261e
ianmi <ian.github@pronovost.net>
2023-04-04 19:56:40 -0400

    playground controller added

--- PR2L/playground.py
+++ PR2L/playground.py
@@ -1,7 +1,7 @@
 #playground provides Dummy environments and tools for testing basic algorithms 
 import gymnasium as gym
 import numpy as np
-
+import pygame
 
 class DeterministicObservations:
     """
@@ -126,5 +126,141 @@ class EpisodeLength:
         else:
             self.count +=1
             return False
+
+
+class Controller:
+    def __init__(self, action_shape, step_callback=print):
+        assert isinstance(action_shape, (list, tuple, np.ndarray))
+        if isinstance(action_shape, np.ndarray) == False:
+            self.action_shape = np.array(action_shape, copy=False)
+        self.ndim = self.action_shape.ndim
+        self.outputs = []
+        self.inputs = []
+        self.step_callback = step_callback
+        pygame.init()
+        self.screen = pygame.display.set_mode((400, 400))
+        pygame.display.set_caption("Controller")
+
+    def loadController(self, inputs, outputs):
+        assert isinstance(inputs, (tuple, list))
+        self.inputs = inputs
+        assert isinstance(outputs[0][0], float)
+        assert isinstance(outputs[0][1], (list, tuple))
+        self.outputs = outputs
+
+    def makeController(self):
+        self.__pycollect_inputs(self.inputs)
+        size = len(self.inputs)
+        self.__pycollect_outputs(self.ndim, self.inputs, self.outputs, size)
+        print("\nSuccessfully initialized controller: ")
+        print("\nInputs: ", self.inputs)
+        print("\nOutputs: ", self.outputs)
+
+    def play(self, delay):
+        # Dictionary to store currently pressed keys and their outputs
+        pressed_keys = {}
+
+        # Create a list with the same length as outputs and values set to 0
+        while True:
+            pygame.time.wait(delay)
+            action = np.zeros(shape=self.action_shape)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.KEYDOWN:
+                    if event.unicode in self.inputs:
+                        input_index = self.inputs.index(event.unicode)
+                        output = self.outputs[input_index]
+                        pressed_keys[event.unicode] = output[0]
+                if event.type == pygame.KEYUP:
+                    if event.unicode in pressed_keys:
+                        del pressed_keys[event.unicode]
+
+            # Update the output_list with the currently pressed keys and their outputs
+            for key, value in pressed_keys.items():
+                for i, inp in enumerate(self.inputs):
+                    if key == inp:
+                        action[self.outputs[i][1]] = self.outputs[i][0]
+
+            self.step_callback(action)
+    
+    @staticmethod
+    def __pycollect_outputs(dim, inputs, outputs, size):
+        
+        while True:
+            #condition
+            if len(outputs) == size:
+                break
+            # wait for user input
+            print("\nCurrent controller: ")
+            print(inputs)
+            print(outputs)
+            out = []
+            cur_val = ''
+            cur_b = True
+            print("\nEnter the output value for input at index: ", len(outputs))
+            while cur_b:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        keyname = pygame.key.name(event.key)
+                        if keyname.isdigit() or event.unicode == '.' or event.unicode == '-':
+                            cur_val += event.unicode
+                            print("\nPress ENTER to confirm or BACKSPACE to start-over")
+                            print("\nCurrent output: ", cur_val)
+                        elif event.key == pygame.K_RETURN:
+                            try: 
+                                out.append(float(cur_val))
+                            except:
+                                print("\nOutput must be int or float!")
+                                cur_val = ''
+                                continue
+                            else:
+                                cur_b = False
+                        elif event.key == pygame.K_BACKSPACE:
+                            cur_val = ''
+                            print("\nEnter the output value for input at index: ", len(outputs))
+            
+            cur_b = True
+            indices = []
+            print("\nEnter the output's corresponding indices: ")
+            while cur_b:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        keyname = pygame.key.name(event.key)
+                        if keyname.isdigit():
+                            if len(indices) == dim:
+                                print("\nindices are already matching with ndim! Press ENTER to continue or BACKSPACE to start-over:")
+                                continue
+                            indices.append(int(event.unicode))
+                            print("current indices: ", indices)
+                        elif event.key == pygame.K_RETURN:
+                            cur_b = False
+                            out.append(indices)
+                        elif event.key == pygame.K_BACKSPACE:
+                            indices = []
+                            print("current indices: ", indices)
+
+            outputs.append(out)
+
+    @staticmethod
+    def __pycollect_inputs(inputs):
+            print("\nEnter input values to use for controller: ")
+            print(inputs)
+            while True:
+                # wait for user input
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RETURN:
+                            # user pressed Enter, break out of the loop
+                            return
+                        elif event.key == pygame.K_BACKSPACE:
+                            inputs = []
+                            print(inputs)
+                        elif event.key != pygame.K_RETURN:
+                            # add input value to the inputs list
+                            inputs.append(event.unicode)
+                            print(inputs) 
 
 #TODO environment that verifies the contents of exp_source and or buffer
